5e719a00ee0ec5103f7d16fce877948d9940164a
Full Name <kurmunke@gmail.com>
2018-08-31 16:34:51 +0300

    Updates route rules

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ Router::group(['namespace' => 'App\Controllers'], function () {
     Router::get('/login', 'User@login');
     Router::get('/restore', 'User@restore');
     Router::get('/registration', 'User@registration');
-    Router::get('/{userName}', 'User@profile');
-    Router::get('/', 'User@feed');
     Router::get('/settings', 'User@settings');
     Router::get('/404', 'User@page404');
+    Router::get('/{userName}', 'User@profile');
+    Router::get('/', 'User@feed');
 });
