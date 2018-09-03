b4a2a4ba387c097a573d81232e1cfdf7fc799161
Full Name <kurmunke@gmail.com>
2018-09-03 13:48:20 +0300

    add new route rule

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,17 @@ use Framework\Router;
 
 Router::group(['namespace' => 'App\Controllers'], function () {
     Router::get('/login', 'User@login');
+    Router::get('/login.html', 'User@login');
     Router::get('/restore', 'User@restore');
+    Router::get('/restore.html', 'User@restore');
     Router::get('/registration', 'User@registration');
+    Router::get('/registration.html', 'User@registration');
     Router::get('/settings', 'User@settings');
+    Router::get('/settings.html', 'User@settings');
+    Router::get('/feed', 'User@feed');
+    Router::get('/feed.html', 'User@feed');
     Router::get('/404', 'User@page404');
-    
+
     Router::get('/{userName}', 'User@profile');
     Router::get('/', 'User@feed');
 });
