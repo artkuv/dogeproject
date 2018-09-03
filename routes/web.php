76a95f8f8a6300907a612f6ff0679f69388c5633
Full Name <kurmunke@gmail.com>
2018-09-03 21:34:36 +0300

    Update route rules

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,10 @@ use Framework\Router;
 
 Router::group(['namespace' => 'App\Controllers'], function () {
     Router::get('/', 'FeedController@feed');
+    Router::get('/registration', 'RegistrationController@registration');
     Router::get('/login', 'LoginController@login');
     Router::get('/restore', 'RestoreController@restore');
-    Router::get('/registration', 'RegistrationController@registration');
-    Router::get('/settings', 'SettingsController@settings');
     Router::get('/profile/{userName?}', 'ProfileController@profile');
+    Router::get('/settings', 'SettingsController@settings');
     Router::get('/404', 'Page404Controller@page404');
 });
