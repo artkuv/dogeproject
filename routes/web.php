964dd8fb4f885259d7eb6f422ad323667105fa1c
Full Name <kurmunke@gmail.com>
2018-09-19 14:42:28 +0300

    Update route rules

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ Router::group(['namespace' => 'App\Controllers'], function () {
     Router::get('/login', 'LoginController@login');
     Router::post('/login/enter', 'LoginController@enter');
     Router::get('/signout', 'ProfileController@signOut');
-    Router::get('/restore', 'RestoreController@restore');
     Router::match(['get','post'],'/profile/{userName?}', 'ProfileController@profile');
     Router::match(['get','post'],'/settings', 'SettingsController@settings');
     Router::get('/404', 'Page404Controller@page404');
